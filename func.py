750b138f9ed9a4e7e8613d956c1195a5c35a2ce3
test <imamyma765@gmail.com>
2025-12-27 05:24:41 +0300

    add func.py

---
+++ func.py
@@ -0,0 +1,35 @@
+def znak(v):
+    if v > 0: return 1
+    if v < 0: return -1
+    return 0
+
+
+def limit(t):
+    return max(min(t, 1), -1)
+
+def conv(v):
+    return abs(v)*2-1
+
+def maxAbs(v, v2):
+    if abs(v) > abs(v2): return v
+    return v2
+
+def _max(v, v2):
+    if v > v2: return v
+    return -v2
+
+def minAbs2(v, v2):
+    return (1-min(abs(v), abs(v2)))*znak(min(v, v2))
+
+def maxMid(v, v2):
+    if abs(abs(v) - 0.5) < abs(abs(v2) - 0.5): return v
+    return v2
+
+def minMid(v, v2):
+    if abs(abs(v) - 0.5) > abs(abs(v2) - 0.5): return v
+    return v2
+
+def minAbs(v, v2):
+    if abs(v) < abs(v2): return v
+    return v2
+
